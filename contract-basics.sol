f15fecfaec855630e79930cbceff4acc97add3d0
David Dickinson <rnddave@gmail.com>
2022-09-28 12:36:51 +0100

    building up some etherium contracts

--- contract-basics.sol
+++ contract-basics.sol
@@ -9,3 +9,37 @@ contract SecondContract{
         number = number + 10;
     }
 }
+
+contract SecondContract{
+
+    int private number = 100;
+
+    function doSomething() public {
+        number = number + 10;
+    }
+
+    function showSomething() public view returns(int) {
+        return number;
+    }
+}
+
+contract BankContract{
+
+    int private Amount;
+
+    function deposit(int amo) public {
+        Amount = Amount+amo;
+
+    }
+
+    function showBalance() public view returns(int){
+        return Amount;
+    }
+
+    function withdraw(int amo) public {
+        if(amo <= Amount) {
+            Amount-=amo;
+        }
+        
+    }
+}
