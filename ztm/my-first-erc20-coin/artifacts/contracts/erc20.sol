7084c913d4545390d8c0bb7a954175e9cfeb85ca
David Dickinson <rnddave@gmail.com>
2023-03-07 21:48:06 +0000

    building up the ERC20 coin

--- ztm/my-first-erc20-coin/artifacts/contracts/erc20.sol
+++ ztm/my-first-erc20-coin/artifacts/contracts/erc20.sol
@@ -7,7 +7,10 @@ contract ERC20 {
     uint256 public totalSupply;
     string public name;
     string public symbol;
-    uint public decimals;
+    // uint public decimals;
+
+    // we need some where to map our balences
+    mapping(address => uint256) public balanceOf; 
 
     constructor(string memory _name, string memory _symbol) {
         name = _name;
@@ -18,6 +21,18 @@ contract ERC20 {
         return 18;
     }
 
+    function transfer(address recipient, uint256 amount) external returns (bool) {
+        require(recipient != address(0), 'ERC20: transfer to sero address');
+        
+        uint256 senderBalance = balanceOf[msg.sender];
+
+        require(senderBalance >= amount, 'ERC20: transfer amount exceed balance');
+
+        balanceOf[msg.sender] = senderBalance - amount;
+        balanceOf[recipient] += amount;
+
+        return true;
+    }
 }
 
 
