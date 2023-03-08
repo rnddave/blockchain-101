47ed6b5f95461a00a40861792d5ba06ae9187fc2
David Dickinson <rnddave@gmail.com>
2023-03-08 09:45:59 +0000

    I have absolutely no idea what Marcus is teaching, it just jumps all over the place, but this is a complete ERC20 token

--- ztm/my-first-erc20-coin/artifacts/contracts/erc20.sol
+++ ztm/my-first-erc20-coin/artifacts/contracts/erc20.sol
@@ -11,6 +11,7 @@ contract ERC20 {
 
     // we need some where to map our balences
     mapping(address => uint256) public balanceOf; 
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(string memory _name, string memory _symbol) {
         name = _name;
@@ -29,6 +30,48 @@ contract ERC20 {
         require(senderBalance >= amount, 'ERC20: transfer amount exceed balance');
 
         balanceOf[msg.sender] = senderBalance - amount;
+        balanceOf[recipient] += amount;
+
+        return true;
+    }
+
+        function decimals() external pure returns (uint8) {     // declaring it here is cheaper from gas PoV
+        return 18;
+    }
+
+    function transfer(address recipient, uint256 amount) external returns (bool) {
+        return _transfer((msg.sender, recipient, amount);
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
+        uint256 currentAllowance = allowance[sender][msg.sender];
+
+        require(
+            currentAllowance >= amount,
+            "ERC20: transfer not allowed"
+        );
+
+        allowance[sender][msg.sender] = currentAllowance - amount;
+        
+        return _transfer((sender, recipient, amount);
+    }
+
+    function approve(address sender, uint256 amount) external returns(bool) {
+        require(sender != address(0), 'ERC20: approve to the zero address');
+
+        allowance[msg.sender][spender] = amount;
+
+        return true;
+    }
+
+    function _transferAddress(address recipient, uint256 amount) private returns (bool) {
+        require(recipient != address(0), 'ERC20: transfer to sero address');
+        
+        uint256 senderBalance = balanceOf[sender];
+
+        require(senderBalance >= amount, 'ERC20: transfer amount exceed balance');
+
+        balanceOf[sender] = senderBalance - amount;
         balanceOf[recipient] += amount;
 
         return true;
